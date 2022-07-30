001e33ec5eba0a5d39385beb9d226dff87de8961
RedFeaTherXll <yuchi9716@gmail.com>
2022-07-30 20:45:48 +0800

    🥵戰鬥結束Title更新🥵

--- src/mc/waitpvp/settings/event/fighendevent.java
+++ src/mc/waitpvp/settings/event/fighendevent.java
@@ -14,7 +14,7 @@ public class fighendevent implements Listener {
         StrikePracticeAPI api = StrikePractice.getAPI();
         Player p = e.getWinner();
         Player pl = e.getLoser();
-        p.sendTitle(ChatColor.GOLD + "" + ChatColor.BOLD + "勝利！", ChatColor.AQUA + "恭喜，你在這場戰鬥中獲勝了！"); ///贏家TITLES
-        pl.sendTitle(ChatColor.RED + "" + ChatColor.BOLD + "你死了！" , ChatColor.WHITE + "正在將你傳送至大廳..."); ///輸家Titles
+        p.sendTitle(ChatColor.GOLD + "" + ChatColor.BOLD + "勝利！", ChatColor.GREEN + p.getName() + ChatColor.WHITE + " 贏得這場勝利"); ///贏家TITLES
+        pl.sendTitle(ChatColor.RED + "" + ChatColor.BOLD + "你死了！" , ChatColor.RED + p.getName() + ChatColor.WHITE + " 贏得這場勝利"); ///輸家Titles
     }
 }
