0ee340d69a1921735fd0141ec0948c470f1fac11
RedFeaTherXll <yuchi9716@gmail.com>
2022-08-01 23:30:24 +0800

    ✅ 修復了沒有倒數計時的顯示錯誤

--- src/mc/waitpvp/settings/event/fightstartevent.java
+++ src/mc/waitpvp/settings/event/fightstartevent.java
@@ -10,45 +10,54 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 public class fightstartevent implements Listener {
-public Core core;
-public int count = 4;
-public  int Task;
-public fightstartevent(Core core) {
-    this.core = core;
-}
+    public Core core;
+    public int count = 4;
+    public int Task;
+
+    public fightstartevent(Core core) {
+        this.core = core;
+    }
 
     StrikePracticeAPI api = StrikePractice.getAPI();
+
     @EventHandler
     public void gui(DuelStartEvent d) {
         Player p1 = d.getPlayer1();
         Player p2 = d.getPlayer2();
         String kit = d.getKit().getName();
-        if (count != -1) {
-            Task = Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(core, () -> {
-                switch (count) {
-                    case 4:
-                        break;
-                    case 3:
-                        p1.sendTitle("§d§l" + count, "" );
-                        p2.sendTitle("§d§l" + count, "" );
-                        break;
-                    case 2:
-                        p1.sendTitle("§d§l" + count, null );
-                        p2.sendTitle("§d§l" + count, null );
-                        break;
-                    case 1:
-                        p1.sendTitle("§d§l" + count, "§b模式: §a" + kit);
-                        p2.sendTitle("§d§l" + count, "§b模式: §a" + kit);
-                        break;
-                }
-                count--;
-                if(count == -1) {
-                    Bukkit.getServer().getScheduler().cancelTask(Task);
-                    p1.sendTitle("§d§l戰鬥開始了!", "§fGood Luck!");
-                    p2.sendTitle("§d§l戰鬥開始了!", "§fGood Luck!");
-                    this.count = 4;
-                }
-            }, 0, 20L);
+        if (d.getKit().isNoDeathCountdown()) { //battlekit deathcountdown false才有
+            p1.sendTitle("§d§l戰鬥開始了!", "§fGood Luck !");
+            p2.sendTitle("§d§l戰鬥開始了!", "§fGood Luck !");
+            return;
+        } else if (!d.getKit().isNoDeathCountdown()) {
+
+            if (count != -1) {
+                Task = Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(core, () -> {
+                    switch (count) {
+                        case 4:
+                            break;
+                        case 3:
+                            p1.sendTitle("§d§l" + count, "");
+                            p2.sendTitle("§d§l" + count, "");
+                            break;
+                        case 2:
+                            p1.sendTitle("§d§l" + count, null);
+                            p2.sendTitle("§d§l" + count, null);
+                            break;
+                        case 1:
+                            p1.sendTitle("§d§l" + count, "§b模式: §a" + kit);
+                            p2.sendTitle("§d§l" + count, "§b模式: §a" + kit);
+                            break;
+                    }
+                    count--;
+                    if (count == -1) {
+                        Bukkit.getServer().getScheduler().cancelTask(Task);
+                        p1.sendTitle("§d§l戰鬥開始了!", "§fGood Luck !");
+                        p2.sendTitle("§d§l戰鬥開始了!", "§fGood Luck !");
+                        this.count = 4;
+                    }
+                }, 0, 20L);
+            }
         }
     }
 }
